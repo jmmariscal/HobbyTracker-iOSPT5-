fc6281388f2fe9fd0fc2aacd11ade8bca0d9a216
Juan M Mariscal <j.m.mariscal03@gmail.com>
2020-01-30 20:32:48 -0800

    First commit

---
+++ HobbyTracker (iOSPT5)/FriendsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HobbyTracker (iOSPT5)
+//
+//  Created by Juan M Mariscal on 1/30/20.
+//  Copyright © 2020 Juan M Mariscal. All rights reserved.
+//
+
+import UIKit
+
+class FriendsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
